bb8a6d32a2b7b5227ed94091f13109bf8c0992c2
iwgx <wahabiputra@gmail.com>
2021-04-27 08:15:16 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const mqtt = require('mqtt')
+const axios = require('axios')
+
+const app = express()
+
+const client = mqtt.connect("mqtt://202.148.1.57:1883", {
+  username: "app-smartlaundrysystem",
+  password: "CcxohtT0HSWn6QzEX6YsV48J9KyES7"
+})
+
+client.on("connect", () => {
+  console.log("Connected to Xirka server!")
+
+  client.subscribe('ESP32ToSubscribe', (error) => {
+    console.log("Subscribed to Xirka device")
+  })
+})
+
+client.on("message", (topic, message) => {
+  console.log("Received message", { topic, message })
+})
+
+app.post("/confirm", (request, response) => {
+  if (client.connected) {
+    client.publish("ESP32ToPublish", {
+      payload: {
+        status: 1
+      }
+    })
+  }
+})
+
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:${3000}`)
+})
